0848dae36ae6ed96c0d6c263697978535578d7d3
shreedhar desai <desaishreedhar09@gmail.com>
2020-03-11 15:11:38 +0530

    Updates

---
+++ precache-manifest.206b23a22755642ed70aaec6f4449b06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "065938670560f3616db77a878cd3b81c",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "4593738967eaa141eade",
+    "url": "/weather/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "da92ca3982279474e6e7",
+    "url": "/weather/static/css/main.6331bd3b.chunk.css"
+  },
+  {
+    "revision": "4593738967eaa141eade",
+    "url": "/weather/static/js/2.2b1e03ef.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/weather/static/js/2.2b1e03ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da92ca3982279474e6e7",
+    "url": "/weather/static/js/main.2fd39d58.chunk.js"
+  },
+  {
+    "revision": "21baee1ee3fded119a76",
+    "url": "/weather/static/js/runtime-main.6859beea.js"
+  }
+]);
